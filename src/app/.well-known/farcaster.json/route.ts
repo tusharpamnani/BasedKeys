66c52f74c9901a111285916528199b4929375d9b
tusharpamnani <tusharpamnani1@gmail.com>
2025-07-30 20:08:45 +0530

    updated manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -10,19 +10,19 @@ export async function GET() {
     return NextResponse.json({
       "accountAssociation": {
         "header": "eyJmaWQiOjExMzk4NjksInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg0YUU5NjYzRDA0MThFNzdFMTUxOEY3RUU4ZDk5ZjMyODlkNWZBNDdkIn0",
-        "payload": "eyJkb21haW4iOiJzcGVlZGtleXMuZmFyY2FzdGVyLnh5eiJ9",
-        "signature": "MHhhNDI3ZDEwMjYyMmZhMGM5NTdjNjI3NTZjMTU0ZmFmM2M3M2I0YzEzZTY4MjhiNjZlOGUyNDQyNDU3OWQ2YWI3NmQzYjZlYmU0ZjU5YzM3MjA5MDE3MjlhOTU4NjJlY2FjY2NkYzIzOTZjMGViZDFlOTMxOWRlZTg2NjAwYjMwOTFj"
+        "payload": "eyJkb21haW4iOiJzcGVlZGtleXMueHl6In0",
+        "signature": "MHhkZmViZmRlYjEzMzZjOWY0MTc4YTljYjcwMWUxOTVjYzVkYjQyNzkxZmFjY2Y4YzMwOWFmYmZkOThjNmFhMTQxNzkyYmZmMjE4OTIxNDhkNWY4MTAzZjQ2ZTFhYjcwNGU2MmZkOGY3ZGJmZDlmY2I3ZTY3MDc2ZDJjY2M4MWRmOTFi"
       },
       "frame": {
         "version": "1",
         "name": "SpeedKeys",
-        "iconUrl": "https://speedkeys.farcaster.xyz/icon.png",
-        "homeUrl": "https://speedkeys.farcaster.xyz",
-        "imageUrl": "https://speedkeys.farcaster.xyz/image.png",
+        "iconUrl": "https://speedkeys.xyz/icon.png",
+        "homeUrl": "https://speedkeys.xyz",
+        "imageUrl": "https://speedkeys.xyz/image.png",
         "buttonTitle": "Launch Mini App",
-        "splashImageUrl": "https://speedkeys.farcaster.xyz/splash.png",
+        "splashImageUrl": "https://speedkeys.xyz/splash.png",
         "splashBackgroundColor": "#eeccff",
-        "webhookUrl": "https://speedkeys.farcaster.xyz/api/webhook"
+        "webhookUrl": "https://speedkeys.xyz/api/webhook"
       }
     });
   }
