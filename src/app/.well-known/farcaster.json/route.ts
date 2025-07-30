79dc32e741787638ba9e398383aa8d6cbe6960c4
tusharpamnani <tusharpamnani1@gmail.com>
2025-07-30 20:04:49 +0530

    added manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getFarcasterDomainManifest } from '~/lib/utils';
+import { getFarcasterDomainManifest } from '../../../lib/utils';
 
 export async function GET() {
   try {
@@ -7,6 +7,23 @@ export async function GET() {
     return NextResponse.json(config);
   } catch (error) {
     console.error('Error generating metadata:', error);
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({
+      "accountAssociation": {
+        "header": "eyJmaWQiOjExMzk4NjksInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg0YUU5NjYzRDA0MThFNzdFMTUxOEY3RUU4ZDk5ZjMyODlkNWZBNDdkIn0",
+        "payload": "eyJkb21haW4iOiJzcGVlZGtleXMuZmFyY2FzdGVyLnh5eiJ9",
+        "signature": "MHhhNDI3ZDEwMjYyMmZhMGM5NTdjNjI3NTZjMTU0ZmFmM2M3M2I0YzEzZTY4MjhiNjZlOGUyNDQyNDU3OWQ2YWI3NmQzYjZlYmU0ZjU5YzM3MjA5MDE3MjlhOTU4NjJlY2FjY2NkYzIzOTZjMGViZDFlOTMxOWRlZTg2NjAwYjMwOTFj"
+      },
+      "frame": {
+        "version": "1",
+        "name": "SpeedKeys",
+        "iconUrl": "https://speedkeys.farcaster.xyz/icon.png",
+        "homeUrl": "https://speedkeys.farcaster.xyz",
+        "imageUrl": "https://speedkeys.farcaster.xyz/image.png",
+        "buttonTitle": "Launch Mini App",
+        "splashImageUrl": "https://speedkeys.farcaster.xyz/splash.png",
+        "splashBackgroundColor": "#eeccff",
+        "webhookUrl": "https://speedkeys.farcaster.xyz/api/webhook"
+      }
+    });
   }
 }
