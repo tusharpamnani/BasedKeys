4ff0551bdd7978d7f8b9cceafc9b7c3f71480b7b
tusharpamnani <tusharpamnani1@gmail.com>
2025-07-30 20:37:22 +0530

    test

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,29 +1,37 @@
 import { NextResponse } from 'next/server';
 import { getFarcasterDomainManifest } from '../../../lib/utils';
 
-export async function GET() {
-  try {
-    const config = await getFarcasterDomainManifest();
-    return NextResponse.json(config);
-  } catch (error) {
-    console.error('Error generating metadata:', error);
-    return NextResponse.json({
-      "accountAssociation": {
-        "header": process.env.FARCASTER_HEADER,
-        "payload": process.env.FARCASTER_PAYLOAD,
-        "signature": process.env.FARCASTER_SIGNATURE
-      },
-      "frame": {
-        "version": "1",
-        "name": "SpeedKeys",
-        "iconUrl": "https://speedkeys-theta.vercel.app/icon.png",
-        "homeUrl": "https://speedkeys-theta.vercel.app",
-        "imageUrl": "https://speedkeys-theta.vercel.app/image.png",
-        "buttonTitle": "Launch Mini App",
-        "splashImageUrl": "https://speedkeys-theta.vercel.app/splash.png",
-        "splashBackgroundColor": "#eeccff",
-        "webhookUrl": "https://speedkeys-theta.vercel.app/api/webhook"
+function withValidProperties(
+  properties: Record<string, undefined | string | string[]>,
+) {
+  return Object.fromEntries(
+    Object.entries(properties).filter(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.length > 0;
       }
+      return !!value;
+    }),
+  );
+}
+
+export async function GET() {
+  const URL = process.env.NEXT_PUBLIC_URL;
+    return NextResponse.json({
+      accountAssociation: {
+        header: process.env.FARCASTER_HEADER,
+        payload: process.env.FARCASTER_PAYLOAD,
+        signature: process.env.FARCASTER_SIGNATURE,
+      },
+      frame: withValidProperties({
+        version: "1",
+        name: "SpeedKeys",
+        iconUrl: `${URL}/icon.png`,
+        homeUrl: `${URL}`,
+        imageUrl: `${URL}/image.png`,
+        buttonTitle: "Launch Mini App",
+        splashImageUrl: `${URL}/splash.png`,
+        splashBackgroundColor: "#eeccff",
+        webhookUrl: `${URL}/api/webhook`,
+      }),
     });
   }
-}
